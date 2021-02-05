885ecb5c9ecfc83084a013d2f88f6273f9e69339
Nikolai9906 <nicohhhh1@gmail.com>
2021-02-04 22:43:19 -0500

    README and HTML finish

--- src/test/java/edu/escuelaing/arep/stat/StatTest.java
+++ src/test/java/edu/escuelaing/arep/stat/StatTest.java
@@ -17,9 +17,74 @@ public class StatTest extends TestCase {
      * Prueba encargada de evaluar la media con unos valores manualmente insertados
      */
     @Test
-    public void testTrue(){
-        assertTrue(true);
+    public void testMean() {
+        Stat data = new Stat();
+        data.addNumber(15.0);
+        data.addNumber(69.9);
+        data.addNumber(6.5);
+        data.addNumber(22.4);
+        data.addNumber(28.4);
+        data.addNumber(65.9);
+        data.addNumber(19.4);
+        data.addNumber(198.7);
+        data.addNumber(38.8);
+        data.addNumber(138.2);
+        try {
+            assertEquals(data.mean(), 60.32);
+        } catch (LinkedListException e) {
+            assertTrue(false);
+        }
     }
+
+    /**
+     * Prueba encargada de evaluar la media con unos valores provenientes de un documento
+     */
+    @Test
+    public void testMeanFile() {
+        Stat data = new Stat("numbersFile.txt");
+        try {
+            assertEquals(data.mean(), 550.6);
+        } catch (LinkedListException e) {
+            assertTrue(false);
+        }
+
+    }
+
+    /**
+     * Prueba encargada de evaluar la desviación estándar con unos valores manualmente insertados
+     */
+    @Test
+    public void testStddev() {
+        Stat data = new Stat();
+        data.addNumber(15.0);
+        data.addNumber(69.9);
+        data.addNumber(6.5);
+        data.addNumber(22.4);
+        data.addNumber(28.4);
+        data.addNumber(65.9);
+        data.addNumber(19.4);
+        data.addNumber(198.7);
+        data.addNumber(38.8);
+        data.addNumber(138.2);
+        try {
+            assertEquals(data.stddev(), 62.26);
+        } catch (LinkedListException e) {
+            assertTrue(false);
+        }
+    }
+    /**
+     * Prueba encargada de evaluar la desviación estándar con unos valores provenientes de un documento
+     */
+    @Test
+    public void testStddevFile() {
+        Stat data = new Stat("numbersFile.txt");
+        try {
+            assertEquals(data.stddev(), 572.03);
+        } catch (LinkedListException e) {
+            assertTrue(false);
+        }
+    }
+
 
 
 }
